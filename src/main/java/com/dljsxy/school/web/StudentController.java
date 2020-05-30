065eb6c751d2ff3f486f7f9f3afc36dbd367c6a9
ningtao <@$35github>
2020-05-30 23:20:17 +0800

    api

--- src/main/java/com/dljsxy/school/web/StudentController.java
+++ src/main/java/com/dljsxy/school/web/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author ningtao
@@ -33,6 +34,7 @@ public class StudentController {
     }
 
     @RequestMapping("/add-student")
+    @ResponseBody
     public Long addStudent(AddStudentReq req) {
         return studentService.addStudent(req);
     }
