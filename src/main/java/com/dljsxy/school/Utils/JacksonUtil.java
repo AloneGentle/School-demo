662bf40abf9b38838db1ec4f1f0152f8ae5cfb82
ningtao <@$35github>
2020-06-05 15:08:03 +0800

    add commons-lang3

---
+++ src/main/java/com/dljsxy/school/Utils/JacksonUtil.java
@@ -0,0 +1,8 @@
+package com.dljsxy.school.Utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public final class JacksonUtil {
+    public static final ObjectMapper DEFAULT_MAPPER = new ObjectMapper();
+
+}
