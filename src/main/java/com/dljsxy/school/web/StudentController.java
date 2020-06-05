9427a8e6ff13dde82daf70a76e6056b86faed760
ningtao <@$35github>
2020-06-05 15:08:03 +0800

    add commons-lang3

--- src/main/java/com/dljsxy/school/web/StudentController.java
+++ src/main/java/com/dljsxy/school/web/StudentController.java
@@ -1,15 +1,19 @@
 package com.dljsxy.school.web;
 
+import com.dljsxy.school.constant.WebExceptionEnum;
 import com.dljsxy.school.entity.Student;
+import com.dljsxy.school.exception.WebApiException;
 import com.dljsxy.school.model.StudentModel;
 import com.dljsxy.school.service.StudentService;
 import com.dljsxy.school.web.reqRes.AddStudentReq;
+import org.hibernate.exception.GenericJDBCException;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLException;
 
 /**
  * @author ningtao
@@ -37,5 +41,19 @@ public class StudentController {
     @ResponseBody
     public Long addStudent(AddStudentReq req) {
         return studentService.addStudent(req);
+    }
+
+
+    @RequestMapping("/add")
+    @ResponseBody
+    public Long addStudent(Long req) {
+        if (req.equals(1L)) {
+
+            throw new WebApiException(WebExceptionEnum.PARAM_ERROR);
+        } else if (req.equals(2L)) {
+            throw new GenericJDBCException("xxx", new SQLException());
+        } else {
+            throw new RuntimeException();
+        }
     }
 }
