afa956250260d48c07883c7b5fa6eff4ade37b85
ningtao <ningtao@bytedance.com>
2021-01-10 10:50:39 +0800

    fix

--- src/main/java/com/dljsxy/school/web/CourseController.java
+++ src/main/java/com/dljsxy/school/web/CourseController.java
@@ -1,17 +1,14 @@
 package com.dljsxy.school.web;
 
-import com.dljsxy.school.entity.Course;
-import com.dljsxy.school.web.reqRes.AddCourseReq;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 import com.dljsxy.school.constant.WebExceptionEnum;
 import com.dljsxy.school.exception.WebApiException;
-import com.dljsxy.school.model.CourseModel;
 import com.dljsxy.school.service.CourseService;
+import com.dljsxy.school.web.reqRes.AddCourseReq;
 import org.hibernate.exception.GenericJDBCException;
-import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.sql.SQLException;
 
 
@@ -21,17 +18,6 @@ public class CourseController {
     private final CourseService courseService;
 
     public CourseController(CourseService courseService){this.courseService = courseService;}
-
-    @RequestMapping("/hello2")
-    public String hello(Model mv) {
-        CourseModel courseModel = new CourseModel();
-//        user.setName("nt");
-        Course course = courseService.getCourseInfoByName("run");
-        BeanUtils.copyProperties(course, courseModel);
-
-        mv.addAttribute("user", courseModel);
-        return "hello";
-    }
 
     @RequestMapping("/add-course")
     @ResponseBody
